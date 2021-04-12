57dda7a414f70632942c32b40e818261dfe88673
KJH940526 <enddl0121@naver.com>
2021-04-12 15:43:28 +0900

    back-end request mapping

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-    devServer : {
-        port : 3000
-    }
+    devServer: {
+        port: 3000,
+        proxy: {
+            //프록시란? 사전적으로 '대리, 대리인'이라는 의미를 갖고 있다
+            //프로토콜에 있어서 대리 응답이라는 개념으로 이해를 하면 편함.
+            '/api/*': {
+                target: 'http://localhost:8080'
+            }
+        }
+    },
 }
